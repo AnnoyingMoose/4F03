eddafc7e555705de44c00ca11fc5d78be2c42b18
Emmanuel Thiessen <annoyingmoose.th@gmail.com>
2017-01-23 16:04:31 -0500

    Implemented string construction

--- PA1/pa1.c
+++ PA1/pa1.c
@@ -9,10 +9,50 @@
 
 #include <stdlib.h>
 #include <stdio.h>
+#include <time.h>
 #include <pthread.h>
+
+int i, N, L, M;
+char *S;
+int Stail = 0;
+pthread_mutex_t Smutex = PTHREAD_MUTEX_INITIALIZER;
+
+void guaranteedSleep(unsigned int s, unsigned int ns)
+{
+	struct timespec t = {s, ns};
+	while (t.tv_nsec > 0 || t.tv_sec > 0) nanosleep(&t, &t);
+}
+
+void *threadfunc(void* arg)
+{
+	char character = (char)arg;
+
+	// Construct the string
+	while (Stail < M * L)
+	{
+		// Sleep for 100~500 ms
+		guaranteedSleep(0, 100000000 + rand() % 400000000);
+
+		pthread_mutex_lock(&Smutex);
+
+		// Append the current thread's character
+		// (if the string isn't already fully built)
+		if (Stail < M * L)
+			S[Stail++] = character;
+
+		pthread_mutex_unlock(&Smutex);
+	}
+
+	// Verify the selected property
+	// TODO
+
+	return NULL;
+}
 
 int main(int argc, char ** argv)
 {
+	// Command-line argument sanity-checking
+
 	if (argc != 8)
 	{
 		fprintf(stderr, "Usage: %s i N L M c0 c1 c2\n", argv[0]);
@@ -24,11 +64,10 @@ int main(int argc, char ** argv)
 		exit(1);
 	}
 
-	int
-		i = atoi(argv[1]),
-		N = atoi(argv[2]),
-		L = atoi(argv[3]),
-		M = atoi(argv[4]);
+	i = atoi(argv[1]);
+	N = atoi(argv[2]);
+	L = atoi(argv[3]);
+	M = atoi(argv[4]);
 
 	if (i < 0 || i > 3)
 	{
@@ -42,9 +81,17 @@ int main(int argc, char ** argv)
 		exit(1);
 	}
 
+	// We'll assume any non-negative values for L and M are valid
 	if (L < 0) L = -L;
 	if (M < 0) M = -M;
 
+	// TODO: c0, c1, c2
+
+	// Allocate space for the string on the stack (so we don't have to worry about deallocation/lack thereof)
+	char s[L*M];
+	S = s;
+
+	// Same for the thread handles
 	pthread_t threads[N];
 
 	// ...
