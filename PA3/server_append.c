5e0fe04196c0146a2142f5a1db65e46a9ab422da
James Lee <jamesrenhoulee@gmail.com>
2017-03-20 11:52:59 -0400

    creates sockets and connects

--- PA3/server_append.c
+++ PA3/server_append.c
@@ -11,13 +11,10 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <time.h>
-
-#ifdef _OPENMP
-#include <omp.h>
-#else
-#define omp_get_thread_num()  0
-#define omp_get_num_threads() 1
-#endif
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h>
 
 char** currentString;
 int currentIndex = 0;
@@ -31,6 +28,48 @@ int RPC_Append(char c){
 }
 
 // initialize the append server, sending in required parameters
-void RPC_InitAppendServer(int propertyIndex, int segmentLength, int numSegments, char c0, char c1, char c2, char hostName[]){
+void RPC_InitAppendServer(int propertyIndex, int segmentLength, int numSegments, char c0, char c1, char c2, char* hostName){
 	// self setup
+	int sockid, portno, n;
+
+    struct sockaddr_in serv_addr;
+    struct hostent *server;
+
+    char buffer[256];
+    portno = 20000;
+    sockid = socket(AF_INET, SOCK_DGRAM, 0);
+    if (sockid < 0){
+		fprintf(stderr,"Could not open socket\n");
+		exit(1);
+	}
+    server = gethostbyname(*hostName);
+    if (server == NULL) {
+        fprintf(stderr,"No such host\n");
+        exit(0);
+    }
+    bzero((char *) &serv_addr, sizeof(serv_addr));
+    serv_addr.sin_family = AF_INET;
+    bcopy((char *)server->h_addr, 
+         (char *)&serv_addr.sin_addr.s_addr,
+         server->h_length);
+    serv_addr.sin_port = htons(portno);
+	int status = connect(sockid,(struct sockaddr *)&serv_addr,sizeof(serv_addr));
+    if (status < 0){
+		fprintf(stderr,"Could not connect\n");
+		exit(1);
+	}
+    printf("Please enter the message: ");
+    bzero(buffer,256);
+    fgets(buffer,255,stdin);
+    n = write(sockid,buffer,strlen(buffer));
+    if (n < 0){
+		fprintf(stderr, "Could not write to socket\n");
+		exit(1);
+	}
+    return 0;
+	
+}
+
+int main(int argc, char ** argv)
+{
 }
--- PA3/server_verify.c
+++ PA3/server_verify.c
@@ -11,8 +11,12 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <time.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <netinet/in.h>
 
 char** completedString;
+int numThreads, segmentLength, numSegments;
 
 // threads call this on the verify server to retrieve a segment to verify
 char** RPC_GetSeg(int seg){
@@ -21,8 +25,50 @@ char** RPC_GetSeg(int seg){
 }
 
 // initialize the verify server, sending in required parameters
-void RPC_InitVerifyServer(int numThreads, int segmentLength, int numSegments){
+void RPC_InitVerifyServer(int N, int L, int M){
+	numThreads = N;
+	segmentLength = L;
+	numSegments = M;
 	// self setup
+	int sockid, newsockid, portno, clilen;
+	char buffer[256];
+	struct sockaddr_in serv_addr, cli_addr;
+	int n;
+	sockid = socket(AF_INET, SOCK_DGRAM, 0);
+	if (sockid < 0){
+		fprintf(stderr,"Could not open socket\n");
+		exit(1);
+	}
+	bzero((char *) &serv_addr, sizeof(serv_addr));
+	portno = 20000;
+	serv_addr.sin_family = AF_INET;
+	serv_addr.sin_addr.s_addr = INADDR_ANY;
+	serv_addr.sin_port = htons(portno);
+	int status = bind(sockid, (struct sockaddr *) &serv_addr, sizeof(serv_addr));
+	if (status < 0){ 
+		fprintf(stderr,"Could not bind\n");
+		exit(1);
+	}
+	listen(sockid,5);
+	clilen = sizeof(cli_addr);
+	newsockid = accept(sockid, (struct sockaddr *) &cli_addr, &clilen);
+	if (newsockid < 0){
+		fprintf(stderr,"Could not accept\n");
+		exit(1);
+	}
+	bzero(buffer,256);
+	n = read(newsockid,buffer,255);
+	if (n < 0){
+		fprintf(stderr,"Could not read from socket\n");
+		exit(1);
+	}
+	printf("Here is the message: %s\n",buffer);
+	completedString = buffer;
+	return 0; 
 	
 	// initialize UDP socket to recieve S from append server
+}
+
+int main(int argc, char ** argv)
+{
 }
