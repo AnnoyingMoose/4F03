cac5683e981491f5994ccaa0001e0701fdab4afe
Emmanuel Thiessen <annoyingmoose.th@gmail.com>
2017-03-22 22:05:49 -0400

    Replaced server source files with dummy versions

--- PA3/server_append.c
+++ PA3/server_append.c
@@ -8,68 +8,17 @@
  * James Lee            (leejr@mcmaster.ca, MSID#001318125)
  */
 
-#include <stdlib.h>
-#include <stdio.h>
-#include <time.h>
-#include <sys/types.h>
-#include <sys/socket.h>
-#include <netinet/in.h>
-#include <netdb.h>
+#include "server_append.h"
+#include "property.h"
 
-char** currentString;
-int currentIndex = 0;
-
-// threads call this to try and append a character
-int RPC_Append(char c){
-	currentString[currentIndex++] = c;
-	// return 0 if appending success
-	return 0;
-	// return -1 if S is complete
-}
-
-// initialize the append server, sending in required parameters
-void RPC_InitAppendServer(int propertyIndex, int segmentLength, int numSegments, char c0, char c1, char c2, char* hostName){
-	// self setup
-	int sockid, portno, n;
-
-    struct sockaddr_in serv_addr;
-    struct hostent *server;
-
-    char buffer[256];
-    portno = 20000;
-    sockid = socket(AF_INET, SOCK_DGRAM, 0);
-    if (sockid < 0){
-		fprintf(stderr,"Could not open socket\n");
-		exit(1);
-	}
-    server = gethostbyname(*hostName);
-    if (server == NULL) {
-        fprintf(stderr,"No such host\n");
-        exit(0);
-    }
-    bzero((char *) &serv_addr, sizeof(serv_addr));
-    serv_addr.sin_family = AF_INET;
-    bcopy((char *)server->h_addr,
-         (char *)&serv_addr.sin_addr.s_addr,
-         server->h_length);
-    serv_addr.sin_port = htons(portno);
-	int status = connect(sockid,(struct sockaddr *)&serv_addr,sizeof(serv_addr));
-    if (status < 0){
-		fprintf(stderr,"Could not connect\n");
-		exit(1);
-	}
-    printf("Please enter the message: ");
-    bzero(buffer,256);
-    fgets(buffer,255,stdin);
-    n = write(sockid,buffer,strlen(buffer));
-    if (n < 0){
-		fprintf(stderr, "Could not write to socket\n");
-		exit(1);
-	}
-    return 0;
-
-}
-
-int main(int argc, char ** argv)
+int *rpc_append_1(char *c, struct svc_req *req)
 {
+	// TODO
+	return NULL;
+}
+
+void rpc_initappendserver_1(char **args, struct svc_req *req)
+{
+	// TODO
+	return;
 }
--- PA3/server_verify.c
+++ PA3/server_verify.c
@@ -8,67 +8,17 @@
  * James Lee            (leejr@mcmaster.ca, MSID#001318125)
  */
 
-#include <stdlib.h>
-#include <stdio.h>
-#include <time.h>
-#include <sys/socket.h>
-#include <sys/types.h>
-#include <netinet/in.h>
+#include "server_verify.h"
 
-char** completedString;
-int numThreads, segmentLength, numSegments;
-
-// threads call this on the verify server to retrieve a segment to verify
-char** RPC_GetSeg(int seg){
-	char** string = "temp";
-	return string;
+char **rpc_getseg_1(int *tid, struct svc_req *req)
+{
+	// TODO
+	return NULL;
 }
 
 // initialize the verify server, sending in required parameters
-void RPC_InitVerifyServer(int N, int L, int M){
-	numThreads = N;
-	segmentLength = L;
-	numSegments = M;
-	// self setup
-	int sockid, newsockid, portno, clilen;
-	char buffer[256];
-	struct sockaddr_in serv_addr, cli_addr;
-	int n;
-	sockid = socket(AF_INET, SOCK_DGRAM, 0);
-	if (sockid < 0){
-		fprintf(stderr,"Could not open socket\n");
-		exit(1);
-	}
-	bzero((char *) &serv_addr, sizeof(serv_addr));
-	portno = 20000;
-	serv_addr.sin_family = AF_INET;
-	serv_addr.sin_addr.s_addr = INADDR_ANY;
-	serv_addr.sin_port = htons(portno);
-	int status = bind(sockid, (struct sockaddr *) &serv_addr, sizeof(serv_addr));
-	if (status < 0){
-		fprintf(stderr,"Could not bind\n");
-		exit(1);
-	}
-	listen(sockid,5);
-	clilen = sizeof(cli_addr);
-	newsockid = accept(sockid, (struct sockaddr *) &cli_addr, &clilen);
-	if (newsockid < 0){
-		fprintf(stderr,"Could not accept\n");
-		exit(1);
-	}
-	bzero(buffer,256);
-	n = read(newsockid,buffer,255);
-	if (n < 0){
-		fprintf(stderr,"Could not read from socket\n");
-		exit(1);
-	}
-	printf("Here is the message: %s\n",buffer);
-	completedString = buffer;
-	return 0;
-
-	// initialize UDP socket to recieve S from append server
-}
-
-int main(int argc, char ** argv)
+void rpc_initverifyserver_1(char **args, struct svc_req *req)
 {
+	// TODO
+	return;
 }
