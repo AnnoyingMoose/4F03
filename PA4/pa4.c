5d294d3017e224e9af3319398ee55f2a56d9c4a8
James Lee <jamesrenhoulee@gmail.com>
2017-04-03 00:45:07 -0400

    copied MPI starting code over from slides

--- PA4/pa4.c
+++ PA4/pa4.c
@@ -12,15 +12,40 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-#include "blur.h"
+#include <blur.h>
+
+#include <string.h>
+#include <mpi.h>
 
 int main(int argc, char **argv)
 {
+	char greeting[MAX_STRING];
+	int comm_sz;
+	int my_rank;
+	
 	if (argc < 2)
 	{
 		fprintf(stderr, "No image specified\n");
 		exit(1);
 	}
+	
+	MPI_Init(NULL, NULL);
+	MPI_Comm_size(MPI_COMM_WORLD, &comm_sz);
+	MPI_Comm_rank(MPI_COMM_WORLD, &my_rank);
+	
+	if (my_rank != 0){
+		fprintf(stderr, "Greetings from process %d of %d", my_rank, comm_sz);
+		MPI_Send(greeting, strlen(greeting)+1, MPI_CHAR, 0, 0, MPI_COMM_WORLD);
+	}
+	else{
+		fprintf(stderr, "Greetings from process %d of %d", my_rank, comm_sz);
+		for (int i = 1; i < comm_sz; i++){
+			MPI_Recv(greeting, MAX_STRING, MPI_CHAR, i, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+		}
+	}
+	
+	MPI_Finalize();
+	return 0;
 
 	// ...
 }
