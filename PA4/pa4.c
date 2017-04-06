feeaac244c4d417b8b1e7799949872c093ceda84
Emmanuel Thiessen <annoyingmoose.th@gmail.com>
2017-04-06 11:39:20 -0400

    Revert "separated into threads to do the work (however there is a division by 0 error somewhere)"
    This reverts commit ee063bd1c83edafe44717bfc0e7b4d2278f150d5.

--- PA4/pa4.c
+++ PA4/pa4.c
@@ -11,7 +11,7 @@
 
 #include <stdlib.h>
 #include <stdio.h>
-#include <mpi.h>
+//~ #include <mpi.h>
 
 #include "ppmFile.h"
 #include "blur.h"
@@ -20,11 +20,11 @@ int main(int argc, char **argv)
 {
 	int comm_sz = 1, my_rank = 0;
 
-	MPI_Init(&argc, &argv);
-	//atexit(MPI_Finalize);
+	//~ MPI_Init(&argc, &argv);
+	//~ atexit(MPI_Finalize);
 
-	MPI_Comm_size(MPI_COMM_WORLD, &comm_sz);
-	MPI_Comm_rank(MPI_COMM_WORLD, &my_rank);
+	//~ MPI_Comm_size(MPI_COMM_WORLD, &comm_sz);
+	//~ MPI_Comm_rank(MPI_COMM_WORLD, &my_rank);
 
 	if (argc < 4)
 	{
@@ -33,33 +33,16 @@ int main(int argc, char **argv)
 
 		exit(1);
 	}
-	
+
 	int radius = atoi(argv[1]);
 	Image *srcImage, *dstImage;
 
 	srcImage = ImageRead(argv[2]);
 	dstImage = ImageCreate(srcImage->width, srcImage->height);
 
-	// divide work evenly among non main threads
-	float work = (srcImage->height)/(comm_sz-1);
-	if (my_rank != 0){
-		fprintf(stderr, "Greetings from %d of %d\n", my_rank, comm_sz);	
-		int i, j;
-		for (i = radius; i < (srcImage->height) - radius; i++){
-			for (j = radius; j < srcImage->width - radius; j++){
-				blurPixel(srcImage, dstImage, radius, i, j);
-			}
-		}
-	}
-	else{
-		fprintf(stderr, "Greetings from main\n");
-		// main thread doesnt do anything (i think)
-		
-	}
-	//blurImage(srcImage, dstImage, radius);
+	blurImage(srcImage, dstImage, radius);
 
 	ImageWrite(dstImage, argv[3]);
-	
-	MPI_Finalize();
+
 	return 0;
 }
